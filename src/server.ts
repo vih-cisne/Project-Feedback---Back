7866e87cfd0ccbe1a55b0679d76b8d632678213a
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-05-04 16:45:22 -0300

    ok

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import  express  from 'express'
+
+const app = express()
+
+app.post('/feedbacks', (req,res) => {
+    return res.send('Hello world')
+})
+
+app.listen(3333, () => {
+    console.log('HTTP server running!')
+})
